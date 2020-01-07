3e148ca964cc9c28cbec0166f8a662282f27fcf5
mohamedisakr <mohamedisakr@gmail.com>
2020-01-07 16:01:42 +0200

    broken our homepage into smaller components, finished styling menu-item component

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+function Directory() {
+  const [sections, setSections] = useState([
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      linkUrl: "shop/hats"
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      linkUrl: "shop/jackets"
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      linkUrl: "shop/sneakers"
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      linkUrl: "shop/womens"
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
+      linkUrl: "shop/mens"
+    }
+  ]);
+
+  return (
+    <div className="menu-directory">
+      {sections.map(({ id, title, imageUrl, size }) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      ))}
+    </div>
+  );
+}
+
+export default Directory;
